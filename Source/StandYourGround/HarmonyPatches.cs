90c2e3978842000de629194b08f80534140b6763
zyllle <zyll3on@gmail.com>
2020-08-02 15:53:04 -0600

    Initial commit

---
+++ Source/StandYourGround/HarmonyPatches.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using HarmonyLib;
+
+namespace StandYourGround
+{
+    public static class HarmonyPatches
+    {
+        [StaticConstructorOnStartup]
+        public static class StandYourGround
+        {
+            static StandYourGround()
+            {
+                Harmony harmony = new Harmony("zylle.StandYourground");
+                Log.Message("Initializing Stand Your Ground");
+                harmony.PatchAll();
+            }
+           
+        }
+    }
+
+
+    [HarmonyPatch(typeof(RimWorld.PawnComponentsUtility), "AddAndRemoveDynamicComponents")]
+    internal static class HostilityResponsePatch
+    {
+        static void Postfix(Pawn pawn, bool actAsIfSpawned = false)
+        {
+            if (pawn.RaceProps.Humanlike && !pawn.Dead && pawn.Faction.IsPlayer)
+            {
+                if (pawn.WorkTagIsDisabled(WorkTags.Violent))
+                {
+                    //pawn.playerSettings.hostilityResponse = RimWorld.HostilityResponseMode.Ignore;
+                    pawn.playerSettings.hostilityResponse = StandYourGroundSettings.pacifistDefault;
+
+                }
+                else
+                {
+                    pawn.playerSettings.hostilityResponse = StandYourGroundSettings.violentDefault;
+                    //pawn.playerSettings.hostilityResponse = RimWorld.HostilityResponseMode.Attack;
+                }
+
+                //if (StandYourGroundSettings.flagAreaRestriction)
+                //{
+                //    if(pawn.Map != null)
+                //    {
+                //        Log.Message("... checking area");
+
+                //        List<Pawn> colonists = pawn.Map.mapPawns.FreeColonists;
+
+                //        if (colonists.Count > 1)
+                //        {
+                //            Area area = colonists.Where(c => c != pawn).GroupBy(c => c.playerSettings.AreaRestriction)
+                //                 .Select(y => new { Area = y.Key, Count = y.Count() }).OrderByDescending(g => g.Count).First().Area;
+
+                //            if (area != null)
+                //            {
+                //                Log.Message("Found area " + area.Label);
+                //                Log.Message("Found area " + area);
+
+                //                pawn.playerSettings.AreaRestriction = area;
+
+                //                Log.Message("Pawn assigned to " + pawn.playerSettings.AreaRestriction.Label);
+                //            }
+                            
+
+                //        }
+                //    }
+
+
+                //}
+
+            }
+        }
+    }
+
+}
---
+++ Source/StandYourGround/StandYourGroundSettings.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using UnityEngine;
+
+namespace StandYourGround
+{
+    public class StandYourGroundSettings : ModSettings
+    {
+        //public static bool flagAreaRestriction = true;
+        public static RimWorld.HostilityResponseMode violentDefault = RimWorld.HostilityResponseMode.Attack;
+        public static RimWorld.HostilityResponseMode pacifistDefault = RimWorld.HostilityResponseMode.Flee;
+
+        public override void ExposeData()
+        {
+            //Scribe_Values.Look(ref flagAreaRestriction, "flagAreaRestriction", true);
+            Scribe_Values.Look(ref violentDefault, "violentDefault", RimWorld.HostilityResponseMode.Attack);
+            Scribe_Values.Look(ref pacifistDefault, "pacifistDefault", RimWorld.HostilityResponseMode.Flee);
+        }
+    }
+
+
+    public class StandYourGroundMod : Mod
+    {
+        StandYourGroundSettings settings;
+
+        public StandYourGroundMod(ModContentPack content) : base(content)
+        {
+            this.settings = GetSettings<StandYourGroundSettings>();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            inRect.width = 450f;
+            Listing_Standard listing = new Listing_Standard();
+            listing.Begin(inRect);
+
+            //listing.CheckboxLabeled("ZSYG_AreaRestriction".Translate(), ref StandYourGroundSettings.flagAreaRestriction, "ZSYG_AreaRestrictionTooltip".Translate());
+            listing.Label("ZSYG_DefaultHostilityResponse".Translate());
+
+            List <RimWorld.HostilityResponseMode> responses = new List<RimWorld.HostilityResponseMode>();
+            responses.Add(RimWorld.HostilityResponseMode.Attack);
+            responses.Add(RimWorld.HostilityResponseMode.Flee);
+            responses.Add(RimWorld.HostilityResponseMode.Ignore);
+
+            //RimWorld.HostilityResponseModeUtility.GetLabel(RimWorld.HostilityResponseMode.Attack);
+            if(listing.ButtonTextLabeled("ZSYG_Default".Translate(), RimWorld.HostilityResponseModeUtility.GetLabel(StandYourGroundSettings.violentDefault)))
+            {
+                List<FloatMenuOption> responseList = new List<FloatMenuOption>();
+                foreach(RimWorld.HostilityResponseMode h in responses)
+                {
+                    responseList.Add(new FloatMenuOption(RimWorld.HostilityResponseModeUtility.GetLabel(h), delegate { StandYourGroundSettings.violentDefault = h; }));
+                }
+                Find.WindowStack.Add(new FloatMenu(responseList));
+
+            }
+
+            List<RimWorld.HostilityResponseMode> paxResponses = new List<RimWorld.HostilityResponseMode>();
+            paxResponses.Add(RimWorld.HostilityResponseMode.Flee);
+            paxResponses.Add(RimWorld.HostilityResponseMode.Ignore);
+
+            if (listing.ButtonTextLabeled("ZSYG_Pacifist".Translate(), RimWorld.HostilityResponseModeUtility.GetLabel(StandYourGroundSettings.pacifistDefault)))
+            {
+                List<FloatMenuOption> responseList = new List<FloatMenuOption>();
+                foreach (RimWorld.HostilityResponseMode h in paxResponses)
+                {
+                    responseList.Add(new FloatMenuOption(RimWorld.HostilityResponseModeUtility.GetLabel(h), delegate { StandYourGroundSettings.pacifistDefault = h; }));
+                }
+                Find.WindowStack.Add(new FloatMenu(responseList));
+
+            }
+            listing.End();
+        }
+
+        public override string SettingsCategory()
+        {
+            return "ZSYG_ModName".Translate();
+        }
+    }
+}
